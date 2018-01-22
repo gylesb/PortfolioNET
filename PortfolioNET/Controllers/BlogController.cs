a54777336b05579e1a8fb24b71bdad24f9c8a767
Gyles Batara <gyles.batara@live.com>
2018-01-22 08:48:26 -0800

    Add Models, Controllers, and Index pages

--- PortfolioNET/Controllers/BlogController.cs
+++ PortfolioNET/Controllers/BlogController.cs
@@ -1,3 +1,57 @@
-﻿<? xml version="1.0" encoding="utf-8" ?>
-<configuration>
-</configuration>
+﻿using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using PortfolioNET.Models;
+using System.Threading.Tasks;
+using PortfolioNET.ViewModels;
+
+namespace PortfolioNET.Controllers
+{
+	[Authorize]
+	public class PostController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+		private readonly UserManager<ApplicationUser> _userManager;
+
+		public PostController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+		{
+			_userManager = userManager;
+			_db = db;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+			var currentUser = await _userManager.FindByIdAsync(userId);
+			return View(_db.Posts.Where(x => x.User.Id == currentUser.Id));
+		}
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Create(Post post)
+		{
+			var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+			var currentUser = await _userManager.FindByIdAsync(userId);
+			post.User = currentUser;
+			_db.Posts.Add(post);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult DisplayObject()
+		{
+			return Json(_db.Posts.Where(x => x.Id == 1));
+		}
+
+		public IActionResult HelloAjax()
+		{
+			return Content("Hello from the controller!", "text/plain");
+		}
+	}
+}
